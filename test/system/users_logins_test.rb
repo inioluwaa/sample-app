be98624b666f43607c9e0da1c283829c47775ee9
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-08 14:23:12 +0100

    test layout changes

---
+++ test/system/users_logins_test.rb
@@ -0,0 +1,17 @@
+require 'application_system_test_case'
+
+class UsersLoginsTest < ApplicationSystemTestCase
+  #   test 'log in users' do
+  #     visit login_path
+  #     fill_in 'Email', with: 'example@railstutorial.org'
+  #     fill_in 'Password', with: 'foobar'
+  #
+  #     click_on 'Log in'
+  #   end
+
+  # test "visiting the index" do
+  #   visit users_logins_url
+  #
+  #   assert_selector "h1", text: "UsersLogin"
+  # end
+end
