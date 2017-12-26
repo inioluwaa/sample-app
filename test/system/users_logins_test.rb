85d68d5918d0ed266274cc50d104cfe32388a156
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-26 21:14:17 +0100

    add edit form and use partial

--- test/system/users_logins_test.rb
+++ test/system/users_logins_test.rb
@@ -1,14 +1,15 @@
 require 'application_system_test_case'
 
 class UsersLoginsTest < ApplicationSystemTestCase
-  #   test 'log in users' do
-  #     visit login_path
-  #     fill_in 'Email', with: 'example@railstutorial.org'
-  #     fill_in 'Password', with: 'foobar'
-  #
-  #     click_on 'Log in'
-  #   end
+=begin
+  test 'log in users' do
+    visit login_path
+    fill_in 'Email', with: 'example@railstutorial.org'
+    fill_in 'Password', with: 'foobar'
 
+    click_on 'Log in'
+  end
+=end
   # test "visiting the index" do
   #   visit users_logins_url
   #
