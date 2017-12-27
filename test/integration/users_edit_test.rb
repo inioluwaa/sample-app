84ff8f13b4fd7cbdb2e097eb3436077aebe8d1ce
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-28 00:30:43 +0100

    friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,5 +32,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    log_in_as @user
+    assert_redirected_to edit_user_path(@user)
+    name = 'Michael Example'
+    email = 'michael@example.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
 
