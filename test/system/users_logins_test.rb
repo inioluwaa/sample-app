cb9af0666e73c917daee7f04022730693e21ddfb
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-06 10:31:12 +0100

    remove system test

--- test/system/users_logins_test.rb
+++ test/system/users_logins_test.rb
@@ -1,15 +1,6 @@
 require 'application_system_test_case'
 
 class UsersLoginsTest < ApplicationSystemTestCase
-=begin
-  test 'log in users' do
-    visit login_path
-    fill_in 'Email', with: 'example@railstutorial.org'
-    fill_in 'Password', with: 'foobar'
-
-    click_on 'Log in'
-  end
-=end
   # test "visiting the index" do
   #   visit users_logins_url
   #
